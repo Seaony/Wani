872f30a735db144ffe44c2ee44ac62ccf525fc04
Seaony <seaony@outlook.com>
2026-08-26 14:09:23 +0800

    Initial commit

---
+++ macOS/Wani/WaniApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaniApp.swift
+//  Wani
+//
+//  Created by seaony on 2026/8/26.
+//
+
+import SwiftUI
+
+@main
+struct WaniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
